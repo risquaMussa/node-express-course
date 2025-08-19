722f6bdb2a7f5d5174471062d066cdb7b249a651
risquaMussa <rismussa@gmail.com>
2025-08-18 21:45:36 -0400

    set up database config file for MongoDB connection

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,29 @@
-console.log('Task Manager App')
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+const connectDB = require("./db/connect");
+
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+// routes
+app.get("/", (req, res) => {
+  res.send("Task Manager API");
+});
+
+const start = async () => {
+  try {
+    console.log("Mongo URI:", process.env.MONGO_URI); //debugging
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
