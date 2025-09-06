244aea9f8371c9dbe917dfd95ef5e36fdfaab5a5
risquaMussa <rismussa@gmail.com>
2025-09-06 00:45:13 -0400

    (store-api): implement product query filters, sorting, field selection

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,1 +1,27 @@
-console.log('04 Store API')
+//console.log('04 Store API')
+require("dotenv").config();
+require("express-async-errors");
+
+const express = require("express");
+const app = express();
+
+const connectDB = require("./db/connect");
+
+const productsRouter = require("./routes/products");
+app.use("/api/v1/products", productsRouter);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    //console.log("MONGO_URI:", process.env.MONGO_URI);
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
--- 04-store-api/starter/db/connect.js
+++ 04-store-api/starter/db/connect.js
@@ -1,12 +1,15 @@
-const mongoose = require('mongoose')
+//i changed the connected db becuase i installed the latest version of mangooes
 
-const connectDB = (url) => {
-  return mongoose.connect(url, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
-}
+const mongoose = require("mongoose");
 
-module.exports = connectDB
+const connectDB = async (url) => {
+  try {
+    await mongoose.connect(url);
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
